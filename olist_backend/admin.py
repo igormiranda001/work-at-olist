45aca5f6fc62261cad9340aa7a2413c832272aaf
igormiranda001 <igormiranda001@yahoo.com.br>
2020-04-18 00:00:19 -0300

    Models Author and Book added in admin

--- olist_backend/admin.py
+++ olist_backend/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from olist_backend.models.author import Author
+from olist_backend.models.book import Book
 
-# Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
