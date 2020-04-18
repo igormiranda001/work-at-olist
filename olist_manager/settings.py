3edfe700cfbc3626e3ef45f10cee3ec4c1fd4333
igormiranda001 <igormiranda001@yahoo.com.br>
2020-04-17 22:47:07 -0300

    Renamed folder olist to olist_manager

--- olist_manager/settings.py
+++ olist_manager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'adieqo!kvr_e4ut5@1zsvi1bclh_2g*^edq)l$&e49#rxsu&l!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
