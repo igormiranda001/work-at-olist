c374f48828b5f2e1abe209935bbf95494663fb76
igormiranda001 <igormiranda001@yahoo.com.br>
2020-04-18 00:01:49 -0300

    Updated name of project in settings files

--- olist_manager/asgi.py
+++ olist_manager/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for olist project.
+ASGI config for olist_manager project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olist.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'olist_manager.settings')
 
 application = get_asgi_application()
