8e3f0a91b60b47a0b73216dd06631b5dea94667d
igormiranda001 <igormiranda001@yahoo.com.br>
2020-04-19 21:10:43 -0300

    Updated urls with CRUD routers

--- olist_manager/urls.py
+++ olist_manager/urls.py
@@ -18,8 +18,23 @@ from django.urls import path
 from django.conf.urls import include
 from django.conf.urls import url
 from .router import router
+from olist_frontend.views.home import HomeTemplateView
+from olist_frontend.views.author import AuthorTemplateView, AuthorCreateTemplateView, AuthorUpdateView, AuthorDeleteView
+from olist_frontend.views.book import BookTemplateView, BookCreateTemplateView, BookUpdateView, BookDeleteView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    
+    path('', HomeTemplateView.as_view(), name="home"),
+    
+    path('authors/', AuthorTemplateView.as_view(), name="authors"),
+    path('createauthor/', AuthorCreateTemplateView.as_view(), name="create_author"),
+    path('authors/<int:pk>', AuthorUpdateView.as_view(), name="update_author"),
+    path('deleteauthor/<int:pk>', AuthorDeleteView.as_view(), name="delete_author"),
+    
+    path('books/', BookTemplateView.as_view(), name="books"),
+    path('createbook/', BookCreateTemplateView.as_view(), name="create_book"),
+    path('books/<int:pk>', BookUpdateView.as_view(), name="update_book"),
+    path('deletebook/<int:pk>', BookDeleteView.as_view(), name="delete_book"),
 ]
