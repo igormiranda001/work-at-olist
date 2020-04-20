247ea2b7cd41cc2ac0ff35aa24e74e28c184d5f3
igormiranda001 <igormiranda001@yahoo.com.br>
2020-04-19 21:09:37 -0300

    Include django tweaks in app list of settings file

--- olist_manager/settings.py
+++ olist_manager/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'olist_backend',
     'django_filters',
     'bootstrapform',
-    'rest_framework'
+    'rest_framework',
+    'widget_tweaks'
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL='/static/'
+STATIC_ROOT='/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static"),
+]
