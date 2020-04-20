1f0b523a50534402280fde1aa16ffb1c5f87abae
igormiranda001 <igormiranda001@yahoo.com.br>
2020-04-20 11:51:38 -0300

    Insert gunicorn settings to deploy app in heroku

--- olist_manager/settings.py
+++ olist_manager/settings.py
@@ -1,5 +1,6 @@
 import os
 from decouple import config
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,5 +122,5 @@ STATIC_ROOT='/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR, "static"),
 ]
-import django_heroku
+
 django_heroku.settings(locals())
